b33b967987df511eef3a07250a123c0c5b1699ea
Meghana <meghana.madhyastha@gmail.com>
2020-04-10 19:49:51 +0000

    Add JSONReader class.

---
+++ src/json_reader.cpp
@@ -0,0 +1,8 @@
+#include "config.h"
+#include "stat_node.h"
+#include "json_reader.h"
+#include <vector>
+
+std::vector<std::vector<StatNode<float, float>>> JSONReader::convertToBins(){
+}
+
--- src/json_reader.h
+++ src/json_reader.h
@@ -1,6 +1,17 @@
 #ifndef JSON_R
 #define JSON_R
 
+#include "config.h"
+#include "stat_node.h"
+#include <vector>
+
+class JSONReader{
+    std::string filename;
+    std::string package_name;
+    public:
+        JSONReader(): filename(Config::getValue("modelfilename")), package_name(Config::getValue("package"){}
+        std::vector<std::vector<StatNode<float, float>>> convertToBins();
+};
 
 
 
