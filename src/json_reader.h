a3410881c6769a9413ed8d08bbe603bc5cff648a
Meghana <meghana.madhyastha@gmail.com>
2020-04-10 20:02:37 +0000

    Fixed import bug

--- src/json_reader.h
+++ src/json_reader.h
@@ -3,13 +3,14 @@
 
 #include "config.h"
 #include "stat_node.h"
+#include"../jsonlib/json.hpp"
 #include <vector>
 
 class JSONReader{
     std::string filename;
     std::string package_name;
     public:
-        JSONReader(): filename(Config::getValue("modelfilename")), package_name(Config::getValue("package"){}
+        JSONReader(): filename(Config::getValue("modelfilename")), package_name(Config::getValue("package")){}
         std::vector<std::vector<StatNode<float, float>>> convertToBins();
 };
 
