ce51b24ba4d92b2380127e0fe412ca883f5532f6
Meghana <meghana.madhyastha@gmail.com>
2020-04-11 04:38:34 +0000

    jsonreader somewhat working

--- src/json_reader.cpp
+++ src/json_reader.cpp
@@ -4,26 +4,33 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
+#include "config.h"
 
 using json = nlohmann::json;
+
 void JSONReader::convertToBins(std::vector<std::vector<StatNode<float, float>>> &bins){
+    const int parse_num = 8; 
+    //std::string model_filename = Config::getValue("modelfilename");
     std::ifstream ifs("/root/pacset/models/rf.json");
     json o = json::parse(ifs); 
-    //each estimator
-    int count = 0;
+    int count;
     json temp2;
     for (auto i: o["estimators_"]){
         json temp = json::parse(i.dump());
         count = 0;
         for(auto j: temp){
-            if(count == 8){
+            if(count == parse_num){
                 temp2 = json::parse(j.dump());
+                auto a = temp2["nodes"];
+                for (auto b: a){
+                    for(auto c: b){
+                        std::cout<<c<<"\n";
+                    }
+                }
+                break;
             }
             ++count;
         }
-        std::cout<<temp2<<"\n";
         break;
     }
-
 }
-    
