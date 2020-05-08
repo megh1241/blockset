9121198cd1e9378cf23c7ba911d3bf66972c89b3
Meghana Madhyastha <meghana.madhyastha@gmail.com>
2020-05-08 23:33:58 +0000

    Added basic runscript

---
+++ runscript_basic.sh
@@ -0,0 +1,3 @@
+
+./exe  --mode both --layout bfs --intertwine 1 --modelfilename ~/pacset/models/cifar128.json --datafilename ~/pacset/data/cifar10.csv --numthreads 1 --package sklearn --algorithm randomforest --task classification
+
