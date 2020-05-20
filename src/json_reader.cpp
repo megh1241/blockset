636aeb631429788f13815e418f96c61a0ce60e68
Meghana <meghana.madhyastha@gmail.com>
2020-05-20 04:15:26 +0000

    [WIP] Handle regression in json_reader.

--- src/json_reader.cpp
+++ src/json_reader.cpp
@@ -30,7 +30,63 @@ int JSONReader<T, F>::populateBinSizes(
 
 
 template<typename T, typename F>
-void JSONReader<T, F>::removeLeafNodes(std::vector<std::vector<StatNode<T, F>>> &bins, 
+void JSONReader<T, F>::removeRegLeafNode(std::vector<std::vector<StatNode<T, F>>> &bins, 
+        std::vector<std::vector<StatNode<T, F>>> temp_ensemble ){
+    std::vector<std::map<int, int>> id_to_index_vec;
+    std::map<int, int> id_to_index;
+    std::vector<std::map<int, int>> oldidx_to_newidx_vec;
+    std::map<int, int> oldidx_to_newidx;
+    std::vector<StatNode<T, F>> temp_bin;
+    int node_counter = 0;
+    int bin_size = 0;
+    int left, right, newleft, newright;
+    int num_bins = temp_ensemble.size();
+
+    for(int i=0; i<num_bins; ++i){
+        bin_size = temp_ensemble[i].size();
+        for(int j=0; j<bin_size; ++j){
+            if(temp_ensemble[i][j].getLeft() != -1 && temp_ensemble[i][j].getRight() != -1){
+                if(temp_ensemble[i][temp_ensemble[i][j].getLeft()].getLeft() == -1){
+                    classn = temp_ensemble[i][temp_ensemble[i][j].getLeft()].getFeature();
+                    temp_ensemble[i][j].setLeft(classn);
+                }
+                if(temp_ensemble[i][temp_ensemble[i][j].getRight()].getLeft() == -1){
+                    classn = temp_ensemble[i][temp_ensemble[i][j].getRight()].getFeature();
+                    temp_ensemble[i][j].setRight(classn);
+                }
+                id_to_index[temp_ensemble[i][j].getID()] = temp_bin.size();
+                temp_bin.push_back(temp_ensemble[i][j]);
+                oldidx_to_newidx[j] = temp_bin.size()-1 ;
+            }
+        }
+
+        id_to_index_vec.push_back(id_to_index);
+        id_to_index.clear();
+        oldidx_to_newidx_vec.push_back(oldidx_to_newidx);
+        oldidx_to_newidx.clear();
+        bins.push_back(temp_bin);
+        temp_bin.clear();
+    }
+
+    int nbins = bins.size();
+    for(int i=0; i<nbins; ++i){
+        for(int j=num_classes; j<bins[i].size(); ++j){
+            left = bins[i][j].getLeft();
+            right = bins[i][j].getRight();
+            if(left >= num_classes){
+                newleft = id_to_index_vec[i][left];
+                bins[i][j].setLeft(newleft);
+            }
+            if(right >= num_classes){
+                newright = id_to_index_vec[i][right];
+                bins[i][j].setRight(newright);
+            }
+        }
+    }
+}
+
+template<typename T, typename F>
+void JSONReader<T, F>::removeClassLeafNodes(std::vector<std::vector<StatNode<T, F>>> &bins, 
         std::vector<std::vector<StatNode<T, F>>> temp_ensemble ){
     int classnum = 0;
     std::vector<std::map<int, int>> id_to_index_vec;
@@ -64,7 +120,7 @@ void JSONReader<T, F>::removeLeafNodes(std::vector<std::vector<StatNode<T, F>>> 
                 oldidx_to_newidx[j] = temp_bin.size()-1 ;
             }
         }
-        
+
         id_to_index_vec.push_back(id_to_index);
         id_to_index.clear();
         oldidx_to_newidx_vec.push_back(oldidx_to_newidx);
@@ -95,6 +151,8 @@ void JSONReader<T, F>::convertSklToBins(std::vector<std::vector<StatNode<T, F>>>
         std::vector<int> &bin_sizes, 
         std::vector<std::vector<int>> &bin_start,
         std::vector<int> &bin_node_sizes){
+
+    std::string task = Config::getValue("task");
     const int parse_num = 8; 
     int count;
     std::string model_filename = Config::getValue("modelfilename");
@@ -102,14 +160,11 @@ void JSONReader<T, F>::convertSklToBins(std::vector<std::vector<StatNode<T, F>>>
     json rf_json_model = json::parse(ifs); 
     json node_json;
 
-    int num_classes = (int) rf_json_model["estimators_"].at(0)["n_classes_"];
-    Config::setConfigItem("numclasses", std::to_string(num_classes));
 
     //Get the number of trees
     auto meta_params = rf_json_model["params"];
     json param_j = json::parse(meta_params.dump());
     int num_trees = param_j["n_estimators"];
-
     int num_bins = populateBinSizes(bin_sizes, num_trees);
 
     //reserve memory for bins
@@ -121,8 +176,14 @@ void JSONReader<T, F>::convertSklToBins(std::vector<std::vector<StatNode<T, F>>>
     int tree_offset = 0, bin_number = 0, tree_num_in_bin = 0;
     std::vector<StatNode<T, F>> temp_bin;
 
-    for(int i=0; i<num_classes; ++i)
-        temp_bin.push_back(StatNode<T, F>(-1, i, -1, -1, -1, -1, -1));
+    if (task.compare(std::string("classification")) == 0) {
+        int num_classes = (int) rf_json_model["estimators_"].at(0)["n_classes_"];
+        Config::setConfigItem("numclasses", std::to_string(num_classes));
+        for(int i=0; i<num_classes; ++i)
+            temp_bin.push_back(StatNode<T, F>(-1, i, -1, -1, -1, -1, -1));
+    }
+    else
+            temp_bin.push_back(StatNode<T, F>(-1, 0, -1, -1, -1, -1, -1));
 
     tree_offset = temp_bin.size();
     //Note: temp_ensemble contains the leaf nodes as a separate node.
@@ -165,19 +226,26 @@ void JSONReader<T, F>::convertSklToBins(std::vector<std::vector<StatNode<T, F>>>
 
             //This is a leaf node. Populate the feature attr with class
             else {
-                auto class_card_arr = values.at(node_counter).at(0);
-                class_num = 0;
-                max = -1;
-                for (auto ele: class_card_arr){
-                    if((int)ele > max){
-                        max = (int)ele;
-                        max_idx = class_num;
+                if (task.compare(std::string("classification")) == 0) {
+                    auto class_card_arr = values.at(node_counter).at(0);
+                    class_num = 0;
+                    max = -1;
+                    for (auto ele: class_card_arr){
+                        if((int)ele > max){
+                            max = (int)ele;
+                            max_idx = class_num;
+                        }
+                        ++class_num;
                     }
-                    ++class_num;
+                    feature = max_idx;
+                    id = temp_bin.size();
+                    temp_bin.emplace_back(left, right, feature, threshold, cardinality, id, 1);
                 }
-                feature = max_idx;
-                id = temp_bin.size();
-                temp_bin.emplace_back(left, right, feature, threshold, cardinality, id, 1);
+                //in case of regression, there are no "classes"
+                else {
+                    feature = node.at(2);
+                    temp_bin.emplace_back(left, right, feature, threshold, cardinality, id, 1);
+                }
             }
             ++node_counter;
         }
@@ -190,13 +258,22 @@ void JSONReader<T, F>::convertSklToBins(std::vector<std::vector<StatNode<T, F>>>
             tree_num_in_bin = 0;
             temp_ensemble.push_back(temp_bin); 
             temp_bin.clear();
-            for(int i=0; i<num_classes; ++i)
-                temp_bin.push_back(StatNode<T, F>(-1, i, -1, -1, -1, -1, -1));
+            if (task.compare(std::string("classification")) == 0) {
+                for(int i=0; i<num_classes; ++i)
+                    temp_bin.push_back(StatNode<T, F>(-1, i, -1, -1, -1, -1, -1));
+            }
+            else {
+                    temp_bin.push_back(StatNode<T, F>(-1, 0, -1, -1, -1, -1, -1));
+            }
         }
         tree_offset = temp_bin.size();
     }
-    
-    removeLeafNodes(bins, temp_ensemble );
+
+    if (task.compare(std::string("classification")) == 0)
+        removeClassLeafNodes(bins, temp_ensemble );
+    else
+        removeRegLeafNodes(bins, temp_ensemble);
+
     //populate the tree root indices (bin_start)
     //TODO: this is inefficient, do this check in removeLeafNodes!
     int counter = 0;
