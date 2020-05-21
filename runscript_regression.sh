710581e388076826cfa035a92c2e180a1865eafd
Meghana <meghana.madhyastha@gmail.com>
2020-05-21 16:22:52 +0000

    Add regression run script.

---
+++ runscript_regression.sh
@@ -0,0 +1,3 @@
+
+./exe  --mode both --layout bfs --intertwine 1 --modelfilename ~/pacset/models/reg.json --datafilename ~/pacset/data/reg.csv --numthreads 1 --package sklearn --algorithm randomforest --task regression 
+
