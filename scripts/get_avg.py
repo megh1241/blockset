591795e81dfe8f1b57093cb72fe88d7ffd1c4b65
Meghana <meghana.madhyastha@gmail.com>
2020-05-31 20:21:29 +0000

    Modify get_avg script.

--- scripts/get_avg.py
+++ scripts/get_avg.py
@@ -2,11 +2,10 @@ import numpy as np
 import sys
 
 path = sys.argv[1]
+delim = sys.argv[2]
 
-b0 = np.genfromtxt(path, delimiter='\n')
+b0 = np.genfromtxt(path, delimiter=delim)
 
-
-#b0 = b0[~np.isnan(b0)]
-b0 = b0[~np.isnan(b0)][0:4]
+b0 = b0[~np.isnan(b0)]
 
 print(np.mean(b0))
