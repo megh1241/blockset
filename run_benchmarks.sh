f2a33381d5c6f68f6af765df4c38c6eb170e8f22
Ubuntu <ubuntu@ip-172-31-20-20.ec2.internal>
2020-06-05 03:02:23 +0000

    Update scripts

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -1,20 +1,21 @@
-./exe  --mode both --layout classblockstat --intertwine 1 --modelfilename ~/pacset/models/mnist.json --datafilename ~/pacset/data/mnist.csv --numthreads 1 --package sklearn --algorithm randomforest --task classification
-./exe  --mode both --layout blockstat --intertwine 1 --modelfilename ~/pacset/models/mnist.json --datafilename ~/pacset/data/mnist.csv --numthreads 1 --package sklearn --algorithm randomforest --task classification
-./exe  --mode both --layout wdfs --intertwine 1 --modelfilename ~/pacset/models/mnist.json --datafilename ~/pacset/data/mnist.csv --numthreads 1 --package sklearn --algorithm randomforest --task classification
-./exe  --mode both --layout bfs --intertwine 1 --modelfilename ~/pacset/models/mnist.json --datafilename ~/pacset/data/mnist.csv --numthreads 1 --package sklearn --algorithm randomforest --task classification
+
+./exe  --mode both --layout binwdfs --intertwine 4 --blocksize 2048 --modelfilename ~/pacset/models/mnist.json --datafilename ~/pacset/data/mnist.csv --numthreads 1 --package sklearn --algorithm randomforest --task classification
+./exe  --mode both --layout binblockstat --intertwine 4 --blocksize 2048 --modelfilename ~/pacset/models/mnist.json --datafilename ~/pacset/data/mnist.csv --numthreads 1 --package sklearn --algorithm randomforest --task classification
+#./exe  --mode both --layout binclassblockstat --intertwine 5 --modelfilename ~/pacset/models/mnist.json --datafilename ~/pacset/data/mnist.csv --numthreads 1 --package sklearn --algorithm randomforest --task classification
+#./exe  --mode both --layout binbfs --intertwine 5 --modelfilename ~/pacset/models/mnist.json --datafilename ~/pacset/data/mnist.csv --numthreads 1 --package sklearn --algorithm randomforest --task classification
+
+#./exe  --mode both --layout classblockstat --intertwine 1 --modelfilename ~/pacset/models/mnist.json --datafilename ~/pacset/data/mnist.csv --numthreads 1 --package sklearn --algorithm randomforest --task classification
+#./exe  --mode both --layout blockstat --intertwine 1 --modelfilename ~/pacset/models/mnist.json --datafilename ~/pacset/data/mnist.csv --numthreads 1 --package sklearn --algorithm randomforest --task classification
+#./exe  --mode both --layout wdfs --intertwine 1 --modelfilename ~/pacset/models/mnist.json --datafilename ~/pacset/data/mnist.csv --numthreads 1 --package sklearn --algorithm randomforest --task classification
+#./exe  --mode both --layout dfs --intertwine 1 --modelfilename ~/pacset/models/mnist.json --datafilename ~/pacset/data/mnist.csv --numthreads 1 --package sklearn --algorithm randomforest --task classification
 
 
-./exe  --mode both --layout binbfs --intertwine 6 --modelfilename ~/pacset/models/mnist.json --datafilename ~/pacset/data/mnist.csv --numthreads 1 --package sklearn --algorithm randomforest --task classification
-./exe  --mode both --layout binwdfs --intertwine 6 --modelfilename ~/pacset/models/mnist.json --datafilename ~/pacset/data/mnist.csv --numthreads 1 --package sklearn --algorithm randomforest --task classification
-./exe  --mode both --layout binclassblockstat --intertwine 6 --modelfilename ~/pacset/models/mnist.json --datafilename ~/pacset/data/mnist.csv --numthreads 1 --package sklearn --algorithm randomforest --task classification
-./exe  --mode both --layout binblockstat --intertwine 6 --modelfilename ~/pacset/models/mnist.json --datafilename ~/pacset/data/mnist.csv --numthreads 1 --package sklearn --algorithm randomforest --task classification
-
-mv logs logs_mnist
-mkdir logs
+#mv logs logs_mnist
+#mkdir logs
 
 
-./exe  --mode both --layout classblockstat --intertwine 1 --modelfilename ~/pacset/models/reg.json --datafilename ~/pacset/data/reg.csv --numthreads 1 --package sklearn --algorithm randomforest --task regression
-./exe  --mode both --layout blockstat --intertwine 1 --modelfilename ~/pacset/models/reg.json --datafilename ~/pacset/data/reg.csv --numthreads 1 --package sklearn --algorithm randomforest --task regression
-./exe  --mode both --layout wdfs --intertwine 1 --modelfilename ~/pacset/models/reg.json --datafilename ~/pacset/data/reg.csv --numthreads 1 --package sklearn --algorithm randomforest --task regression
-./exe  --mode both --layout dfs --intertwine 1 --modelfilename ~/pacset/models/reg.json --datafilename ~/pacset/data/reg.csv --numthreads 1 --package sklearn --algorithm randomforest --task regression
-./exe  --mode both --layout bfs --intertwine 1 --modelfilename ~/pacset/models/reg.json --datafilename ~/pacset/data/reg.csv --numthreads 1 --package sklearn --algorithm randomforest --task regression
+#./exe  --mode both --layout classblockstat --intertwine 1 --modelfilename ~/pacset/models/reg.json --datafilename ~/pacset/data/reg.csv --numthreads 1 --package sklearn --algorithm randomforest --task regression
+#./exe  --mode both --layout blockstat --intertwine 1 --modelfilename ~/pacset/models/reg.json --datafilename ~/pacset/data/reg.csv --numthreads 1 --package sklearn --algorithm randomforest --task regression
+#./exe  --mode both --layout wdfs --intertwine 1 --modelfilename ~/pacset/models/reg.json --datafilename ~/pacset/data/reg.csv --numthreads 1 --package sklearn --algorithm randomforest --task regression
+#./exe  --mode both --layout dfs --intertwine 1 --modelfilename ~/pacset/models/reg.json --datafilename ~/pacset/data/reg.csv --numthreads 1 --package sklearn --algorithm randomforest --task regression
+#./exe  --mode both --layout bfs --intertwine 1 --modelfilename ~/pacset/models/reg.json --datafilename ~/pacset/data/reg.csv --numthreads 1 --package sklearn --algorithm randomforest --task regression
--- runscript_basic.sh
+++ runscript_basic.sh
@@ -1,20 +1,30 @@
 
-./exe  --mode both --layout classblockstat --intertwine 1 --modelfilename ~/pacset/models/mnist.json --datafilename ~/pacset/data/mnist.csv --numthreads 1 --package sklearn --algorithm randomforest --task classification
+./exe  --mode pack --modelfilename2 /data5/packedmodelbinblock6.bin --format binary --layout binblockstat --intertwine 6 --modelfilename /data5/mnist2048.json --datafilename ~/pacset/data/mnist.csv --numthreads 1 --package sklearn --algorithm randomforest --task classification
+
+mv metadata.txt /data5/metadatabinblock6.txt
+
+
+./exe  --mode pack --format binary --layout binwdfs --modelfilename2 /data5/packedmodelbinwdfs6.bin --intertwine 6 --modelfilename /data5/mnist2048.json --datafilename ~/pacset/data/mnist.csv --numthreads 1 --package sklearn --algorithm randomforest --task classification
+
+mv metadata.txt /data5/metadatabinwdfs6.txt
+
+
+./exe  --modelfilename2 /data5/packedmodelblock.bin --mode pack --layout blockstat --intertwine 1 --modelfilename /data5/mnist2048.json --datafilename ~/pacset/data/mnist.csv --numthreads 1 --package sklearn --algorithm randomforest --task classification
+
+mv metadata.txt /data5/metadatablock.txt
+
+./exe  --mode pack --layout wdfs --intertwine 1 --modelfilename /data5/mnist2048.json --modelfilename2 /data5/packedmodelwdfs.bin --datafilename ~/pacset/data/mnist.csv --numthreads 1 --package sklearn --algorithm randomforest --task classification
+mv metadata.txt /data5/metadatawdfs.txt
 #rm packedmodel.txt
 #rm metadata.txt
-#./exe  --mode both --layout blockstat --intertwine 1 --modelfilename ~/pacset/models/mnist.json --datafilename ~/pacset/data/mnist.csv --numthreads 1 --package sklearn --algorithm randomforest --task classification
-
-#./exe  --mode both --layout classblock --intertwine 1 --modelfilename ~/pacset/models/mnist.json --datafilename ~/pacset/data/mnist.csv --numthreads 1 --package sklearn --algorithm randomforest --task classification
-rm packedmodel.txt
-rm metadata.txt
 
 #./exe  --mode both --layout binblock --intertwine 1 --modelfilename ~/pacset/models/mnist.json --datafilename ~/pacset/data/mnist.csv --numthreads 1 --package sklearn --algorithm randomforest --task classification
-rm packedmodel.txt
-rm metadata.txt
+#rm packedmodel.txt
+#rm metadata.txt
 
 #./exe  --mode both --layout binclassblock --intertwine 1 --modelfilename ~/pacset/models/mnist.json --datafilename ~/pacset/data/mnist.csv --numthreads 1 --package sklearn --algorithm randomforest --task classification
-rm packedmodel.txt
-rm metadata.txt
+#rm packedmodel.txt
+#rm metadata.txt
 
 
 #./exe  --mode both --layout bfs --intertwine 1 --modelfilename ~/pacset/models/cifar128.json --datafilename ~/pacset/data/cifar10.csv --numthreads 1 --package sklearn --algorithm randomforest --task classification
