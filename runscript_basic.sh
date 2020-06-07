956774fcf0095267c898d685ad15fa27440884ff
Meghana Madhyastha <meghana.madhyastha@gmail.com>
2020-06-07 22:37:09 +0000

    modified packing script

--- runscript_basic.sh
+++ runscript_basic.sh
@@ -1,19 +1,19 @@
 
-./exe  --mode pack --modelfilename2 /data5/packedmodelbinblock6.bin --format binary --layout binblockstat --intertwine 6 --modelfilename /data5/mnist2048.json --datafilename ~/pacset/data/mnist.csv --numthreads 1 --package sklearn --algorithm randomforest --task classification
+./exe  --mode pack --modelfilename2 /data5/packedmodelbinblock6.bin --format binary --layout binblockstat --intertwine 6 --modelfilename /data5/mnist_manual_2048.json --datafilename ~/pacset/data/mnist.csv --numthreads 1 --package sklearn --algorithm randomforest --task classification
 
 mv metadata.txt /data5/metadatabinblock6.txt
 
 
-./exe  --mode pack --format binary --layout binwdfs --modelfilename2 /data5/packedmodelbinwdfs6.bin --intertwine 6 --modelfilename /data5/mnist2048.json --datafilename ~/pacset/data/mnist.csv --numthreads 1 --package sklearn --algorithm randomforest --task classification
+./exe  --mode pack --format binary --layout binwdfs --modelfilename2 /data5/packedmodelbinwdfs6.bin --intertwine 6 --modelfilename /data5/mnist_manual_2048.json --datafilename ~/pacset/data/mnist.csv --numthreads 1 --package sklearn --algorithm randomforest --task classification
 
 mv metadata.txt /data5/metadatabinwdfs6.txt
 
 
-./exe  --modelfilename2 /data5/packedmodelblock.bin --mode pack --layout blockstat --intertwine 1 --modelfilename /data5/mnist2048.json --datafilename ~/pacset/data/mnist.csv --numthreads 1 --package sklearn --algorithm randomforest --task classification
+./exe  --modelfilename2 /data5/packedmodelblock.bin --format binary --mode pack --layout blockstat --intertwine 1 --modelfilename /data5/mnist_manual_2048.json --datafilename ~/pacset/data/mnist.csv --numthreads 1 --package sklearn --algorithm randomforest --task classification
 
 mv metadata.txt /data5/metadatablock.txt
 
-./exe  --mode pack --layout wdfs --intertwine 1 --modelfilename /data5/mnist2048.json --modelfilename2 /data5/packedmodelwdfs.bin --datafilename ~/pacset/data/mnist.csv --numthreads 1 --package sklearn --algorithm randomforest --task classification
+./exe  --mode pack --layout wdfs --intertwine 1 --format binary --modelfilename /data5/mnist_manual_2048.json --modelfilename2 /data5/packedmodelwdfs.bin --datafilename ~/pacset/data/mnist.csv --numthreads 1 --package sklearn --algorithm randomforest --task classification
 mv metadata.txt /data5/metadatawdfs.txt
 #rm packedmodel.txt
 #rm metadata.txt
