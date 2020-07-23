2fd139940bfde9da2fc6c9b39b5425e1b1539132
Meghana Madhyastha <meghana.madhyastha@gmail.com>
2020-07-23 15:24:08 +0000

    Plotting scripts

---
+++ scripts/paper_plot_scripts/generate_plots.sh
@@ -0,0 +1,9 @@
+python3 boxplot_layout.py Blocks violin bin
+python3 boxplot_layout.py Blocks joy bin
+python3 boxplot_layout.py latency violin bin
+python3 boxplot_layout.py latency joy bin
+
+python3 boxplot_layout.py Blocks violin no
+python3 boxplot_layout.py Blocks joy no
+python3 boxplot_layout.py latency violin no
+python3 boxplot_layout.py latency joy no
