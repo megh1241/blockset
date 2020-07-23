31dd85694a8986052a9540efe28e49b64d07ae4d
Meghana Madhyastha <meghana.madhyastha@gmail.com>
2020-07-23 15:21:09 +0000

    Add plotting scripts. Add script to compute the avg of a given subset of numbers in a csv file.

--- scripts/get_avg_part.py
+++ scripts/get_avg_part.py
@@ -6,7 +6,7 @@ delim = sys.argv[2]
 
 b0 = np.genfromtxt(path, delimiter=delim)
 
-b1 = b0[~np.isnan(b0)][0:10]
+b1 = b0[~np.isnan(b0)][0:11]
 #b2 = b0[~np.isnan(b0)][3001:6000]
 
 print(np.mean(b1))
