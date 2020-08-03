e739c01b7cd1187939de04a1075024268f61b206
Meghana Madhyastha <meghana.madhyastha@gmail.com>
2020-08-03 21:58:33 +0000

    Add regression plot scripts

---
+++ scripts/paper_plot_scripts/generate_plots_depth.sh
@@ -0,0 +1,2 @@
+python3 boxplot_depth_cifar.py block violin
+python3 boxplot_depth_cifar.py block joy 
---
+++ scripts/paper_plot_scripts/generate_plots_layout_regression.sh
@@ -0,0 +1,9 @@
+python3 boxplot_layout_regression.py Blocks violin bin
+python3 boxplot_layout_regression.py Blocks joy bin
+python3 boxplot_layout_regression.py latency violin bin
+python3 boxplot_layout_regression.py latency joy bin
+
+python3 boxplot_layout_regression.py Blocks violin no
+python3 boxplot_layout_regression.py Blocks joy no
+python3 boxplot_layout_regression.py latency violin no
+python3 boxplot_layout_regression.py latency joy no
