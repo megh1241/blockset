4837a09f02cf333b7913bbf5666e999469924619
Meghana Madhyastha <meghana.madhyastha@gmail.com>
2020-08-17 18:05:30 +0000

    Added plotting scripts and logs for embedded PACSET

---
+++ scripts/paper_plot_scripts/generate_plots_layout_embedded.sh
@@ -0,0 +1,9 @@
+python3 boxplot_layout_embedded.py Blocks violin bin
+python3 boxplot_layout_embedded.py Blocks joy bin
+python3 boxplot_layout_embedded.py latency violin bin
+python3 boxplot_layout_embedded.py latency joy bin
+
+python3 boxplot_layout_embedded.py Blocks violin no
+python3 boxplot_layout_embedded.py Blocks joy no
+python3 boxplot_layout_embedded.py latency violin no
+python3 boxplot_layout_embedded.py latency joy no
