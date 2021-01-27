d94589f79025801aeecf440e469d24852af0582a
Meghana <meghana.madhyastha@gmail.com>
2021-01-27 01:58:10 +0000

    pack

--- newrun_pack.sh
+++ newrun_pack.sh
@@ -7,5 +7,5 @@ DATA_FILE="/data6/c.csv"
 for val in ${layoutArray[@]}; do
 	PACK_FILE="/data6/packedmodel${val}.bin"
 	META_FILE="/data6/metadata${val}.txt"
-	./exe  --mode pack --logdir 'logs/' --labelcol 10 --format binary  --metadatafilename $META_FILE --layout $val --intertwine 2 --packfilename $PACK_FILE --modelfilename $MODEL_FILE --datafilename $DATA_FILE --numthreads 1 --package sklearn --algorithm randomforest --task regression 
+	./exe  --mode pack --logdir 'logs/' --labelcol 10 --format binary  --metadatafilename $META_FILE --layout $val --intertwine 2 --packfilename $PACK_FILE --modelfilename $MODEL_FILE --datafilename $DATA_FILE --numthreads 16 --package sklearn --algorithm randomforest --task regression 
 	done
