ac8b87ecce0ba4346c0e9871f7ae9e13a21b171e
Ubuntu <ubuntu@ip-172-31-16-106.ec2.internal>
2021-02-04 16:57:51 +0000

    Delete run scripts

--- newrun_copy.sh
+++
@@ -1,5 +0,0 @@
-declare -a layoutArray=("binbfs" "bindfs" "binstatdfs" "binstatblock")
-
-for val in ${layoutArray[@]}; do
-	python3 scripts/copy_files_single_dir.py 10 '/data5/' 'packedmodel'${val}
-done
--- runscript_regression.sh
+++
@@ -1,3 +0,0 @@
-
-./exe  --mode inference --format binary --modelfilename /data5/packedmodelbfs --layout bfs --intertwine 1 --metadatafilename ~/pacset/metadata.txt --datafilename ~/pacset/data/regdata.csv --numthreads 1 --package sklearn --algorithm randomforest --task regression 
-
