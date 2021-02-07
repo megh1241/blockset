ed0ed97b73bde8a57d90c2d0879260de0fb88ae7
Meghana <11406990+megh1241@users.noreply.github.com>
2021-02-07 00:08:31 -0500

    Update train_sklearn.sh

--- train_sklearn.sh
+++ train_sklearn.sh
@@ -22,5 +22,5 @@ TASK='classification'
 FORMAT='json'
 
 #NOTE: RUN python3 scripts/train_and_save_sklearn.py --help for a list of all options
-#Run this script to train the model and save it in a json format for PACSET to pack. One can modify this script to work for XGBoost as well, i.e the model needs to be stored in a JSON format detailed in the script.
+#Run this script to train the model and save it in a json format for BLOCKSET to pack. One can modify this script to work for XGBoost as well, i.e the model needs to be stored in a JSON format detailed in the script.
 python3 scripts/train_and_save_sklearn.py --labelcol $LAB_COL --datadir $DATA_DIR --datafilename $DATA_FILENAME --numtrees $NUM_TREES --algorithm $ALGORITHM --numtest $NUM_TEST --task $TASK --saveformat $FORMAT
